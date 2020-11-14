a7d47c9b501723eb24991c9bdeac57007e19aa5a
z-9527 <137596665@qq.com>
2020-11-14 15:14:21 +0800

    新增button组件

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,13 +1,71 @@
 import React from 'react';
-import Ripple from '../ripple/index';
+import { Ripple, Icon } from '../index';
+import { classnames } from '../_utils/index';
+import './index.less';
 
-function Button() {
+const Loading = () => <div />;
+
+interface IProps {
+  disabled?: boolean;
+  inline?: boolean;
+  loading?: boolean;
+  ripple?: boolean;
+  round?: boolean;
+  icon?: string;
+  type?: 'primary' | 'warning' | 'ghost' | 'default';
+  className?: string;
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+}
+
+function Button(props: IProps) {
+  const {
+    disabled,
+    inline,
+    loading,
+    ripple,
+    round,
+    icon,
+    type,
+    className,
+    style,
+    children,
+    onClick
+  } = props;
+
+  const cls = {
+    'sty-button': true,
+    [className]: className,
+    [`sty-button-${type}`]: type,
+    'sty-button-disabled': disabled,
+    'sty-button-inline': inline,
+    'sty-button-round': round
+  };
+  const iconEl = loading ? (
+    <Loading size={24} className={'sty-button-loading'} />
+  ) : (
+    icon
+  );
   return (
-    <div style={{ height: 100 }}>
-      button
-      <Ripple />
+    <div className={classnames(cls)} style={style} onClick={onClick}>
+      {!disabled && !loading && ripple && (
+        <Ripple className={'sty-button-ripple'} />
+      )}
+      {typeof iconEl === 'string' ? (
+        <Icon className={'sty-button-icon'} type={icon} />
+      ) : (
+        iconEl
+      )}
+      {children && <span className={'sty-button-text'}>{children}</span>}
     </div>
   );
 }
+
+Button.defaultProps = {
+  ripple: true,
+  type: 'default',
+  onClick: () => undefined
+};
 
 export default Button;
--- site/page/Button/index.tsx
+++ site/page/Button/index.tsx
@@ -1,10 +1,56 @@
 import React from 'react';
 import { Button } from '@/components/index';
+import './index.less';
 
 function ButtonDemo() {
   return (
-    <div>
-      <Button />
+    <div className='button-demo'>
+      <div className='demo-block__title'>按钮类型</div>
+      <div className='block'>
+        <Button type='default'>default</Button>
+        <Button type='primary'>primary</Button>
+        <Button type='warning'>warning</Button>
+        <Button type='ghost'>ghost</Button>
+      </div>
+      <div className='demo-block__title'>行内按钮</div>
+      <div className='block'>
+        <Button inline type='default'>
+          default
+        </Button>
+        <Button inline type='primary'>
+          primary
+        </Button>
+        <Button inline type='warning'>
+          warning
+        </Button>
+        <Button inline type='ghost'>
+          ghost
+        </Button>
+        <Button inline round type='ghost'>
+          round
+        </Button>
+      </div>
+      <div className='demo-block__title'>禁用</div>
+      <div className='block'>
+        <Button disabled type='default'>
+          default
+        </Button>
+        <Button disabled type='primary'>
+          primary
+        </Button>
+      </div>
+      <div className='demo-block__title'>加载和图标</div>
+      <div className='block'>
+        <Button inline type='primary' loading>
+          primary
+        </Button>
+        <Button inline type='warning' loading>
+          warning
+        </Button>
+        <Button icon='like-o' inline type='ghost'>
+          ghost
+        </Button>
+      </div>
     </div>
   );
 }
