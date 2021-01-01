690a2aa36511dfdf8b61d9f687523294d5da616e
z-9527 <137596665@qq.com>
2021-01-01 17:59:36 +0800

    picker组件新增级联功能

--- components/picker/PickerPanel.tsx
+++ components/picker/PickerPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { classnames } from '../_utils/index';
 import { PickerPanelProps, CascadeType } from './index';
 import { Loading } from '../index';
